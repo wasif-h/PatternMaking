a9d07e32bebcb71adfa45febd1d3ef30703ae1a8
Wasif Hossain <98389079+wasif-h@users.noreply.github.com>
2023-10-20 12:47:39 +0600

    Create dabbangpattern.cpp

---
+++ dabbangpattern.cpp
@@ -0,0 +1,40 @@
+//<--Wasif_Hossain-->--- Problem Name :
+#include <bits/stdc++.h>
+using namespace std;
+
+//---------------  main function  ----------------- :)
+int main()
+{
+
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        int c1 = 1;
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << c1 << " ";
+            c1++;
+        }
+
+        for (int k = 1; k <= i - 1; k++)
+        {
+            cout << "* ";
+        }
+
+        for (int n = 1; n <= i - 1; n++)
+        {
+            cout << "* ";
+        }
+
+        int c2 = n - i + 1;
+        for (int m = 1; m <= n - i + 1; m++)
+        {
+            cout << c2 << " ";
+            c2--;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
